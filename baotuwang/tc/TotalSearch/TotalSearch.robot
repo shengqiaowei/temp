e32faefa114a89d65dde36cb69c88e8fe2ebadaf
shengqiaowei <1079089578@qq.com>
2019-09-05 12:13:13 +0800

    增加task07作业

--- baotuwang/tc/TotalSearch/TotalSearch.robot
+++ baotuwang/tc/TotalSearch/TotalSearch.robot
@@ -3,4 +3,4 @@ Library  pylib/ReGressionTest.py
 
 *** Test Cases ***
 全站搜索-tc00050
-    search  ppt
+    search  国庆
--- python-Basics/task07/test.py
+++ python-Basics/task07/test.py
@@ -1,7 +1,45 @@
-#fileName
-def putInfoToDict():
-    with open('file1.txt','r') as f:
-        print(f.read())
-        print(type(f.read()))
+import pprint
+import json
+fileDir = 'file1.txt'
+def putInfoToDict(fileName):
+    resDict = {}
+    with open(fileName,'r') as f:
+        #读所有信息,并且根据\n切
+        liens = f.read().splitlines()
+        #print(liens)
+        #遍历，针对每行进行处理
+        for line in liens:
+            #根据,号切
+            temp = line.split(',')
+            #print(temp)
 
-putInfoToDict()
+            #获取课程id
+            lessonId = temp[1].strip()
+
+            #获取学生的id
+            studentId = temp[2].split(')')[0].strip()
+
+            #获取签到时间
+            checkTime = temp[0].split('(')[1].replace("'",'')
+
+            #排版
+            toAdd = {'lessonId':lessonId,
+                     'checkTime':checkTime}
+            #print(toAdd)
+
+            #判断键是否在字典里，不在的话增加键值对
+            if studentId not in resDict:
+                resDict[studentId] = [toAdd]
+            #如果键存在字典里，就增加值
+            else:
+                resDict[studentId].append(toAdd)
+    return resDict
+
+
+
+
+res = putInfoToDict(fileDir)
+
+#print(json.dumps(res,indent=1))
+
+pprint.pprint(res)
