96736b6910e9dd4c71a222489d0f9017a8b9af7f
sqw <1079089578@qq.com>
2019-09-18 00:02:07 +0800

    222

--- taaobao-Seckill/Seckill.py
+++ taaobao-Seckill/Seckill.py
@@ -10,7 +10,7 @@ class Seckill:
 
     def open_browser(self):
         self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(10)
+        #self.wd.implicitly_wait(10)
 
     def close_browser(self):
         self.wd.quit()
@@ -23,6 +23,7 @@ class Seckill:
         if self.wd.find_element_by_link_text('亲，请登录'):
             self.wd.find_element_by_link_text('亲，请登录').click()
             print(f'请尽快扫码登录')
+            time.sleep(5)
             # self.wd.find_element_by_css_selector('#TPL_username_1').send_keys('盛侨威')
             # time.sleep(3)
             # self.wd.find_element_by_css_selector('#TPL_password_1').send_keys('qiao.0706')
@@ -31,41 +32,57 @@ class Seckill:
 
 
     #选中商品
-    def picking(self,ShoppingNumber,times):
-        #打开购物车页面
-        self.wd.get('https://cart.taobao.com/cart.htm')
+    def picking(self,times):
+        # #打开购物车页面
+        # self.wd.get('https://cart.taobao.com/cart.htm')
+        # time.sleep(2)
+        # #勾选商品
+        # self.wd.find_element_by_css_selector("label[for={}]".format(ShoppingNumber)).click()
+        # time.sleep(0.5)
+
+        #进入牛排详情页
+        self.wd.get('https://detail.tmall.com/item.htm?id=582302039775&spm=a21bz.7725273.1998564503.1.c8c33db8Tz0MAp&umpChannel=qianggou&u_channel=qianggou')
         time.sleep(2)
-        #勾选商品
-        self.wd.find_element_by_css_selector("label[for={}]".format(ShoppingNumber)).click()
-        time.sleep(0.5)
 
         while True:
             now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
             print(now)
             #对比时间，时间到就结算
             if now>times:
-                #结算
                 while True:
+                    # 点击立即购买
                     try:
-                        #点击结算
-                        Settlement = self.wd.find_element_by_css_selector('#J_Go')
-                        if Settlement:
-                            Settlement.click()
-                            print('结算成功,准备提交订单')
-                            time.sleep(1)
+                        buy = self.wd.find_element_by_id('J_LinkBuy')
+                        if buy:
+                            print('购买成功')
+                            buy.click()
+                            #time.sleep(1)
                             break
                     except:
-                        print('再次尝试结算')
+                        print('再次进行购买')
+
+                # #结算
+                # while True:
+                #     try:
+                #         #点击结算
+                #         Settlement = self.wd.find_element_by_css_selector('#J_Go')
+                #         if Settlement:
+                #             Settlement.click()
+                #             print('结算成功,准备提交订单')
+                #             time.sleep(1)
+                #             break
+                #     except:
+                #         print('再次尝试结算')
                 #提交订单
                 while True:
                     #提交订单
                     try:
-                        self.wd.refresh()#刷新当前页面
+                        #self.wd.refresh()#刷新当前页面
                         print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
                         order = self.wd.find_element_by_css_selector('.go-btn')
                         if order:
-                            order.click()
                             print('抢购成功，请尽快付款')
+                            order.click()
                             break
                     except:
                         print('再次尝试提交订单')
@@ -76,7 +93,7 @@ try:
     sk = Seckill()
     sk.open_browser()
     sk.login()
-    sk.picking('J_CheckBox_1506156096178','2019-09-17 18:10')
+    sk.picking('2019-09-18 00:00')
 
 except Exception as e:
     print(e)
